13f10ad8a0a021260ceb4be1e84a01623cceb58c
dude <Joni1234@hotmail.dk>
2018-08-27 15:13:59 +0200

    Created base file management

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 </head>
 <body>
     <div class="page-header">
-        <?php require="templates/header.php";?>
+        <?php require("templates/header.php");?>
     </div>
     <div class="page-content">
-        <?php require="pagecontent/index.php";?>
+        <?php require("pagecontent/index.php");?>
     </div>
     <div class="page-footer">
-        <?php require="templates/footer.php";?>
+        <?php require("templates/footer.php");?>
     </div>
 </body>
 
---
+++ templates/header.php
@@ -0,0 +1,3 @@
+<?php
+echo="<h2> haeder </h2>";
+?>
