445e928fe78b38735c11152bed7dd47fcea39c7b
dude <Joni1234@hotmail.dk>
2018-08-30 13:21:11 +0200

    Pc change

--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,3 @@
 <?php
-    echo file_get_contents("C:/Users/Jas/OneDrive - AARHUS TECH/Programmer/Websites/MyWeb/templates/html/footer.html");
+    echo file_get_contents("C:/Users/bruger/OneDrive - AARHUS TECH/Programmer/Websites/MyWeb/templates/html/footer.html");
 ?>
--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,3 @@
 <?php
-    echo file_get_contents("C:/Users/Jas/OneDrive - AARHUS TECH/Programmer/Websites/MyWeb/templates/html/header.html");
+    echo file_get_contents("C:/Users/bruger/OneDrive - AARHUS TECH/Programmer/Websites/MyWeb/templates/html/header.html");
 ?>
