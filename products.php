8072d5cc7af156a8b00628836c0c2dd963ee937d
dude <Joni1234@hotmail.dk>
2018-08-31 14:17:54 +0200

    Added Product div and some examples

--- products.php
+++ products.php
@@ -2,6 +2,7 @@
 <head> 
     <title>Products</title>
     <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/prod.css">
 </head>
 <body>
     <div class="page-sidebar">
