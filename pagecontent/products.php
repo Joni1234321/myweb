c02abf97992d612e33de80808616f0cc37e80a04
dude <Joni1234@hotmail.dk>
2018-09-03 09:51:51 +0200

    PC change

--- pagecontent/products.php
+++ pagecontent/products.php
@@ -1,5 +1,5 @@
 <?php
-    //echo file_get_contents("C:/Users/bruger/OneDrive - AARHUS TECH/Programmer/Websites/MyWeb/pagecontent/html/products.html");
-    echo file_get_contents("C:/Users/Jas/OneDrive - AARHUS TECH/Programmer/Websites/MyWeb/pagecontent/html/products.html");
+    //echo file_get_contents("C:/Users/Jas/OneDrive - AARHUS TECH/Programmer/Websites/MyWeb/pagecontent/html/products.html");
+    echo file_get_contents("C:/Users/bruger/OneDrive - AARHUS TECH/Programmer/Websites/MyWeb/pagecontent/html/products.html");
 
 ?>
