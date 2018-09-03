4fcc462b637317b621ec15c0110a4e3446593c02
dude <Joni1234@hotmail.dk>
2018-09-03 10:03:47 +0200

    Added Profile page

--- profile.php
+++ profile.php
@@ -1,14 +1,15 @@
 <html>
 <head> 
-    <title>Home</title>
+    <title>Profile</title>
     <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/prod.css">
 </head>
 <body>
     <div class="page-sidebar">
         <?php require("templates/sidebar.php");?>
     </div>
     <div class="page-content">
-        <?php require("pagecontent/index.php");?>
+        <?php require("pagecontent/profile.php");?>
     </div>
 </body>
 
