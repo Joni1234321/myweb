c7a3e763d4810e3b6673ae64f6d1dc1c2761c12a
Jonas <Jvh1234567@gmail.com>
2018-09-10 17:53:11 +0200

    Loads products from DB

--- pagecontent/index.php
+++ pagecontent/index.php
@@ -1,5 +1,4 @@
 <?php
-    //echo file_get_contents("C:/Users/Jas/OneDrive - AARHUS TECH/Programmer/Websites/MyWeb/pagecontent/html/index.html");
-    echo file_get_contents("C:/Users/bruger/OneDrive - AARHUS TECH/Programmer/Websites/MyWeb/pagecontent/html/index.html");
+    echo file_get_contents("C:/Users/Jonas/OneDrive - AARHUS TECH/Programmer/Websites/MyWeb/pagecontent/html/index.html");
 
 ?>
--- pagecontent/products.php
+++ pagecontent/products.php
@@ -1,5 +1,40 @@
 <?php
-    //echo file_get_contents("C:/Users/Jas/OneDrive - AARHUS TECH/Programmer/Websites/MyWeb/pagecontent/html/products.html");
-    echo file_get_contents("C:/Users/bruger/OneDrive - AARHUS TECH/Programmer/Websites/MyWeb/pagecontent/html/products.html");
+    echo "  <div id='headline'>
+                <h2>Products</h2>
+            </div>
+            <div id='content'>
+            <div class='productgrid'>";
+    
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "myweb";
 
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    
+    $sql = "SELECT id, thumbnail, title, descr FROM product";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "
+            <a href='products.php'><div class='product' id='" . $row["id"] . "'>
+            <img class='thumbnail' src='". $row["thumbnail"] . "'>
+            <p class='title'>" . $row["title"] . " </p>
+            <p class='description'>" . $row["descr"] . " </p>
+            </div> </a>";
+        }
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
+
+    echo "</div> </div>"
 ?>
